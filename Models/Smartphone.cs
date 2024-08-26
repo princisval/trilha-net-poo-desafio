575adc1d7181b8ef65c0d6dd73c655a98c5ed61a
princisval <christhian.spsilva@gmail.com>
2024-08-26 08:15:59 -0300

    Projeto concluido

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,11 +3,18 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
+        private string Modelo;
+        private string Imei;
+        private int Memoria;
+
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
+            Modelo = modelo;
+            Imei = imei;
+            Memoria = memoria;
             // TODO: Passar os parâmetros do construtor para as propriedades
         }
 
--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
+System.Console.WriteLine("Smartphone Nokia: ");
+Smartphone nokia = new Nokia(numero: "123456", modelo: "Modelo 01", imei: "1212121212", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+System.Console.WriteLine();
+
+System.Console.WriteLine("Smartphone Iphone: ");
+Smartphone iphone = new Iphone(numero: "8912345", modelo: "Iphone 15", imei: "1212121212", memoria: 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("Instagram");
 // TODO: Realizar os testes com as classes Nokia e Iphone
